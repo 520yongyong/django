31239766ff9728c9137fd42205a275ceacfddbd7
yongyong <2066250031@qq.com>
2020-05-18 22:49:30 +0800

    2

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(ri^ku+c*=^uxd$_=at=3m81=*law$oy6de$d#@62m0v3&pel)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ']
 
 
 # Application definition
